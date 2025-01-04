fc24180e626334b2bb3c2c10773efa16607a2c3a
aroy15 <anjonroy57@gmail.com>
2025-01-04 08:44:37 +0600

    Adding templates for our team members

--- src/edit.js
+++ src/edit.js
@@ -4,13 +4,13 @@ import './editor.scss';
 export default function Edit() {
 	return (
 		<div { ...useBlockProps() }>
-			<InnerBlocks 
+			<InnerBlocks
 				allowedBlocks={ [ 'block-course/team-member' ] }
-				template={[
+				template={ [
 					[ 'block-course/team-member' ], // can be passed default value as well like { name: 'John Doe', bio: 'Lorem ipsum' } as second argument of the each block
 					[ 'block-course/team-member' ],
-					[ 'block-course/team-member' ]
-				]}
+					[ 'block-course/team-member' ],
+				] }
 				//templateLock="all" // using "all" can't add, remove and sorting blocks and using "insert" can't add and remove blocks just sorting is allowed
 			/>
 		</div>
